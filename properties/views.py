1369ff4fdc0e0d62d12bf147407994aa33bddfc9
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-08-31 22:47:12 +0300

    refactor: update views add space in between 60 and * and 15

--- properties/views.py
+++ properties/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 
 # Create your views here.
-@cache_page(60*15)   # cache for 15 minutes
+@cache_page(60 * 15)   # cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price", "location"
