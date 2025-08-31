787d1ca99e102c2319d39c414b81132946470d1b
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-08-31 22:52:43 +0300

    Fix property_list view to return JSON with 'data' key
    - Wrap property queryset in a dictionary with data key to meet API schema requirements.
    - Removed  since top-level object is now a dict.
    - Cache decorator for 15 minutes remains intact.

--- properties/views.py
+++ properties/views.py
@@ -3,10 +3,9 @@ from .models import Property
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 
-# Create your views here.
 @cache_page(60 * 15)   # cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price", "location"
     )
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)})
